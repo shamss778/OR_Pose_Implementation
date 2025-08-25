02de6e4509be66662f22f56da2a6e57adb7b367b
Shams <shams.ben.mefteh5@gmail.com>
2025-08-25 15:14:22 +0200

    set the configurations for cifar10 on Resnet

--- config.py
+++ config.py
@@ -1,21 +1,68 @@
 
 import torch
 
+""" Configuration file for Mean Teacher Method """
+
+# save on CPU by default
+device = torch.device("cpu") 
+
+# model initialization
+model = 'ResNet32x32'  # Options: ResNet32x32, ResNet224x224, cifar_shakeshake26, resnext152
+
+# learning rate configuration
+lr = 0.1
+lr_rampup = 0
+initial_lr = 0
+lr_rampdown_epochs = None
+
+# optimizer parameters
+momentum = 0.9
+weight_decay = 0.1e-4
+optim = 'SGD'
+
+# Evaluation
+evaluate = True
+evaluation_epochs = 1
+
+# Training parameters
+start_epoch = 0
+epochs = 100
+
+# Checkpointing
+checkpoint_epochs = 25  # Set to None to disable checkpointing
+saveX = True
+save_path = 'Mean_Teacher_Method/experiments' # base directory to save the model
+dataName = 'cifar10'
+
+# directories and data
 traindir = 'Mean_Teacher_Method/data/dataset/cifar10/train'
-batch_size = 64
+testdir = 'Mean_Teacher_Method/data/dataset/cifar10/test'
+labels = "Mean_Teacher_Method/data/dataset/labels/cifar10.txt" # path to the file containing labels for a subset of training data
+
+
+# type of learnong: fully supervised or semi-supervised
+supervised = False # if True, only use labeled data for training (fully supervised learning)
+
+# if supervised is False, set labeled_batch_size to the desired batch size of labeled data (semi-supervised learning)
+labeled_batch_size = 31 # if None, use standard random sampler (no constraint on
+batch_size = 128 # total batch size (labeled + unlabeled) for semi-supervised learning, or batch size for fully supervised learning
+
+# DataLoader parameters
 num_workers = 4
 pin_memory = False
-testdir = 'Mean_Teacher_Method/data/dataset/cifar10/test'
-labels = "Mean_Teacher_Method/data/dataset/labels/cifar10"
-consistency = 1.0
-consistency_rampup = 100
-lr = 0.002
-# lr_rampup = 0
-# initial_lr = 0.002
-# lr_rampdown_epochs = 200
+
+# consistency loss parameters
+consistency = 50.0/4
+consistency_rampup = 5 # in the original paper, they used 30 for imagenet 
+
+
+# multi-head architecture parameters
 logit_distance_cost = 1 # for supervised = True, set to -1
-supervised = False
-ema_decay = 0.99
-epochs = 200
-weight_decay = 0.0005
-device = torch.device("cpu")
+
+# Exponential Moving Average (EMA) parameters
+ema_decay = 0.999
+
+
+
+
+
