7c06a363ffa114e4b19d4cb9726d0402448abdd2
Shams <shams.ben.mefteh5@gmail.com>
2025-08-25 15:25:44 +0200

    import fix

--- engine/validate.py
+++ engine/validate.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from config import device
-from data import NO_LABEL
+from data.data import NO_LABEL
 
 def validate(eval_loader, model):
     model.eval() # set model to evaluation mode
