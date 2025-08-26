be16dbef9b5cfb0f130f255fc8eef3291de37f3e
Shams <shams.ben.mefteh5@gmail.com>
2025-08-26 14:00:58 +0200

    added config assignement, changed model to convlarge

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 
 import torch
-
+from types import SimpleNamespace
+import os
 """ Configuration file for Mean Teacher Method """
+
+BASE_DIR = os.path.dirname(__file__)
+
+BN = False # whether to use batch normalization or not
+sntg = False # whether to use sntg loss or not
 
 # save on CPU by default
 device = torch.device("cpu") 
 
 # model initialization
-model = 'ResNet32x32'  # Options: ResNet32x32, ResNet224x224, cifar_shakeshake26, resnext152
+model = 'convlarge'  # Options: ResNet32x32, ResNet224x224, cifar_shakeshake26, resnext152
 
 # learning rate configuration
 lr = 0.1
@@ -31,13 +37,13 @@ epochs = 100
 # Checkpointing
 checkpoint_epochs = 25  # Set to None to disable checkpointing
 saveX = True
-save_path = 'Mean_Teacher_Method/experiments' # base directory to save the model
+save_path = 'experiments' # base directory to save the model
 dataName = 'cifar10'
 
 # directories and data
-traindir = 'Mean_Teacher_Method/data/dataset/cifar10/train'
-testdir = 'Mean_Teacher_Method/data/dataset/cifar10/test'
-labels = "Mean_Teacher_Method/data/dataset/labels/cifar10.txt" # path to the file containing labels for a subset of training data
+traindir = 'data/dataset/cifar10/train'
+testdir = 'data/dataset/cifar10/test'
+labels = "data/dataset/labels/cifar10/cifar10.txt" # path to the file containing labels for a subset of training data
 
 
 # type of learnong: fully supervised or semi-supervised
@@ -61,6 +67,45 @@ logit_distance_cost = 1 # for supervised = True, set to -1
 
 # Exponential Moving Average (EMA) parameters
 ema_decay = 0.999
+
+# config.py
+from types import SimpleNamespace
+
+def build_config():
+    return SimpleNamespace(
+        device=device,
+        model=model,
+        lr=lr,
+        lr_rampup=lr_rampup,
+        initial_lr=initial_lr,
+        lr_rampdown_epochs=lr_rampdown_epochs,
+        momentum=momentum,
+        weight_decay=weight_decay,
+        optim=optim,
+        evaluate=evaluate,
+        evaluation_epochs=evaluation_epochs,
+        start_epoch=start_epoch,
+        epochs=epochs,
+        checkpoint_epochs=checkpoint_epochs,
+        saveX=saveX,
+        save_path=save_path,
+        dataName=dataName,
+        traindir=traindir,
+        testdir=testdir,
+        labels=labels,
+        supervised=supervised,
+        labeled_batch_size=labeled_batch_size,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        consistency=consistency,
+        consistency_rampup=consistency_rampup,
+        logit_distance_cost=logit_distance_cost,
+        ema_decay=ema_decay,
+        BN=BN,
+        sntg=sntg
+    )
+
 
 
 
