3592ca0e88e7cb55918ee6da0ebc8072efb171d4
Shams <shams.ben.mefteh5@gmail.com>
2025-08-28 16:34:31 +0200

    fixed indentation

--- engine/validate.py
+++ engine/validate.py
@@ -15,13 +15,13 @@ def validate(eval_loader, model):
             input_var = Variable(input).to(device) # move input to device (CPU)
             target_var = Variable(target).to(device) # move target to device (CPU) 
         
-        labeled_minibatch_size = target_var.ne(NO_LABEL).sum() # number of labeled samples in the minibatch
+            labeled_minibatch_size = target_var.data.ne(NO_LABEL).sum() # number of labeled samples in the minibatch
 
-        assert labeled_minibatch_size > 0, "No labeled samples in the minibatch"
-        output = model(input_var) # forward pass
+            assert labeled_minibatch_size > 0, "No labeled samples in the minibatch"
+            output = model(input_var) # forward pass
 
-        _ , predicted = torch.max(output.data, 1) # get the index of the max log-probability
-        total += target_var.size(0) 
-        correct += (predicted == target_var).sum().item() # count of correct predictions
+            _ , predicted = torch.max(output.data, 1) # get the index of the max log-probability
+            total += target_var.size(0) # total number of samples
+            correct += (predicted == target_var).sum().item() # count of correct predictions
     
     return 100 * correct / total
